d405c0580c1c79a2ff64e41976aae6b1e3b6aa41
jorgemunoz7 <jamunoz9@asu.edu>
2023-03-30 21:11:46 -0700

    Update script.js

--- Assets/java/script.js
+++ Assets/java/script.js
@@ -1,13 +1,36 @@
 function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://dog.ceo/api/breeds/image/random';
+    // fetch requests get a random dog image and a random dog fact
+    var imageUrl = 'https://dog.ceo/api/breeds/image/random';
+    var factUrl = 'http://dog-api.kinduff.com/api/facts';
   
-    fetch(requestUrl)
+    fetch(imageUrl)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data)
+        // Set the src attribute of the img element to the retrieved image
+        document.getElementById('dog-image').setAttribute('src', data.message);
       })
-    }
-getApi();
+      .catch(function (error) {
+        console.error(error);
+      });
+  
+    fetch(factUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Select a random fact from the array of facts and set the text content of the fact element to it
+        var factIndex = Math.floor(Math.random() * data.facts.length);
+        document.getElementById('dog-fact').textContent = data.facts[factIndex];
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+  
+  // Call the getApi function when the page loads
+  window.addEventListener('load', getApi);
+  
+  //Updated  
+  
