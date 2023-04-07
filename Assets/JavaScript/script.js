cb1b5ece49d7c576efafb815d60afd5de0b3900b
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-04-06 20:34:13 -0700

    Created Gif Page and JS

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -7,13 +7,13 @@ function getApi() {
         return response.json();
       })
       .then(function (data) {
-        // Set the src attribute of the img element to the retrieved image
+        //Set the src attribute of the img element to the retrieved image
         console.log(data)
         picBoxes = document.querySelectorAll('.pic');
         picBoxes.forEach((e) => {
           e.setAttribute('src', data.message)
         });
-        // document.getElementById('dog-image').setAttribute('src', data.message);
+        document.getElementById('dog-image').setAttribute('src', data.message);
       })
       .catch(function (error) {
         console.error(error);
@@ -22,28 +22,34 @@ function getApi() {
     //Section for Giphy JS
     let gifURL = 'https://api.giphy.com/v1/gifs/search?api_key=FN2y1TfECIL5ESAWifTW0uzZCqrkRJQg&q=cute dogs';
     
-    gifBoxes = document.querySelectorAll('.gif');
-    console.log(gifBoxes);
-    
     fetch(gifURL)
       .then(response => response.json())
       .then(content => {
         //Logs to check arrays and status
         console.log(content.data);
         console.log('META', content.meta);
-        gifBoxes.forEach((e) => {
-          i = Math.floor(Math.random() * content.data.length)
-          e.setAttribute('src', content.data[i].images.downsized.url);
+        // i = Math.floor(Math.random() * content.data.length);
+        let i = 0;
+        document.getElementById('gif').setAttribute('src', content.data[i].images.original.url);
+        gifbtn = document.getElementById('gifbtn');
+        gifbtn.addEventListener('click', function(){
+        i++;
+        document.getElementById('gif').setAttribute('src', content.data[i].images.original.url);
         });
       })
       .catch(err =>{
         console.error('error');
-      });  
+      });
   
     }
   
   // Call the getApi function when the page loads
   window.addEventListener('load', getApi);
-  //Updated 
 
   
+
+  // gifBoxes.forEach((e) => {
+        //   i = Math.floor(Math.random() * content.data.length)
+        //   e.setAttribute('src', content.data[i].images.downsized.url);
+        // });
+        // gifBoxes = document.querySelectorAll('.gif');
