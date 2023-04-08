0199f770772db46da9db86be2ade048515725b60
Chris Gramer <gramer3514@gmail.com>
2023-04-08 15:08:39 -0700

    Ability to move page to page | Added Adopt html

--- Assets/JavaScript/pic.js
+++ Assets/JavaScript/pic.js
@@ -31,3 +31,50 @@ function getApi() {
   
   // Call the getApi function when the page loads
   window.addEventListener('load', getApi);
+
+
+  const toGifs = document.getElementById('gifs');
+  const toPhotos = document.getElementById('photos');
+  const toAdopt = document.getElementById('adopt');
+  const toLanding = document.getElementById('landingPage');
+  
+  
+  function moveToGifs(event) {
+    event.preventDefault();
+
+    const gifPage = './GifPage.html'
+    location.assign(gifPage);
+    
+  }
+  
+  function moveToPhotos(event) {
+    event.preventDefault();
+
+    const photoPage = './Photos.html'
+    location.assign(photoPage);
+    
+  }
+  
+  // correct this once adopt page is done - coming soon
+  function moveToAdopt(event) {
+    event.preventDefault();
+
+    const adoptPage = './AdoptPage.html'
+    location.assign(adoptPage);
+    
+  }
+
+  // correct this once landing page is done
+  function moveToLanding(event) {
+    event.preventDefault();
+
+    const landingPage = '/Landing Page/index.html'
+    location.assign(landingPage);
+    
+  }
+
+
+  toGifs.addEventListener('click', moveToGifs);
+  toPhotos.addEventListener('click', moveToPhotos);
+  toAdopt.addEventListener('click', moveToAdopt);
+  toLanding.addEventListener('click', moveToLanding);
