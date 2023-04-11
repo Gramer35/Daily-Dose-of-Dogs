6a434d15bdbeaaf200fa48e6a7fef67cb9c133fe
Chris Gramer <gramer3514@gmail.com>
2023-04-10 20:16:02 -0700

    testing again

--- Assets/JavaScript/pic.js
+++ Assets/JavaScript/pic.js
@@ -68,7 +68,7 @@ function getApi() {
   function moveToLanding(event) {
     event.preventDefault();
 
-    const landingPage = '../JavaScript/index.html'
+    const landingPage = './JavaScript/index.html'
     location.assign(landingPage);
     
   }
